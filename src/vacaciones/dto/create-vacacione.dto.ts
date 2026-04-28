133b73d857915fa181d8caf4a5f599484f066765
FLEXUS <diego@flexuselec.com>
2026-04-28 09:51:45 -0600

    Base de datos vacaciones

--- src/vacaciones/dto/create-vacacione.dto.ts
+++ src/vacaciones/dto/create-vacacione.dto.ts
@@ -1,1 +1,150 @@
-export class CreateVacacioneDto {}
+import { IsDateString, IsEnum, IsInt, IsNumber, IsOptional, IsString, Length, Matches, Max, MaxLength, Min } from "class-validator";
+import { SemaforoEnum, TipoEmpleadoEnum } from "../entities/vacacione.entity";
+import { Column } from "typeorm";
+
+export class CreateVacacioneDto {
+    @IsString()
+    @Length(4,4,{
+        message:"El id del empleado debe tener exactamente 4 digitos"
+    })
+    @Matches(/^\d{4}$/, {
+    message: "El id del empleado solo debe contener números",
+    })
+    idempleado:string
+
+    @IsString()
+  @Length(2, 100, {
+    message: "El nombre debe tener entre 2 y 100 caracteres",
+  })
+  @Matches(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/, {
+    message: "El nombre solo puede contener letras y espacios",
+  })
+  nombre: string;
+
+  @IsEnum(TipoEmpleadoEnum, {
+    message: "El tipo de empleado no es válido",
+  })
+  tipoempleado: TipoEmpleadoEnum;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+  })
+  area: string;
+@Column({
+    type: "varchar",
+    length: 100,
+  })
+  puesto: string;
+
+  @IsDateString({}, {
+    message: "La fecha de ingreso debe tener un formato válido",
+  })
+  fechaingreso: string;
+
+  @IsInt({
+    message: "La antigüedad debe ser un número entero",
+  })
+  @Min(0, {
+    message: "La antigüedad no puede ser menor a 0",
+  })
+  @Max(60, {
+    message: "La antigüedad no puede ser mayor a 60",
+  })
+  antiguedad: number;
+
+
+  @IsInt({
+    message: "Los dias derecho debe ser un número entero",
+  })
+  @Min(0, {
+    message: "Los dias derecho no puede ser menor a 0",
+  })
+  @Max(60, {
+    message: "Los dias derecho no puede ser mayor a 60",
+  })
+  diasderecho: number;
+
+
+   @IsDateString({}, {
+    message: "La fecha de ingreso debe tener un formato válido",
+  })
+  iniciocicloactual: string;
+
+  @IsDateString({}, {
+    message: "La fecha de ingreso debe tener un formato válido",
+  })
+  fincicloactual: string;
+
+  @IsNumber(
+    { maxDecimalPlaces: 2 },
+    {
+      message: "El proporcional devengado debe ser un número con máximo 2 decimales",
+    },
+  )
+  @Min(0)
+  @Max(999.99)
+  proporcionaldevengado: number;
+
+    @IsInt({
+    message: "Los dias tomados debe ser un número entero",
+  })
+  @Min(0, {
+    message: "Los dias tomados no puede ser menor a 0",
+  })
+  @Max(60, {
+    message: "Los dias tomados no puede ser mayor a 60",
+  })
+  diastomados: number;
+
+ @IsNumber(
+    { maxDecimalPlaces: 2 },
+    {
+      message: "El saldo disponible debe ser un número con máximo 2 decimales",
+    },
+  )
+  @Min(-9999.99, {
+    message: "El saldo disponible no puede ser menor a -9999.99",
+  })
+  @Max(9999.99, {
+    message: "El saldo disponible no puede ser mayor a 9999.99",
+  })
+  saldodisponible: number;
+
+
+   @IsInt({
+    message: "Los dias por vencer debe ser un número entero",
+  })
+  @Min(0, {
+    message: "Los dias por vencer no puede ser menor a 0",
+  })
+  @Max(60, {
+    message: "Los dias por vencer no puede ser mayor a 60",
+  })
+  diasporvencer: number;
+
+  @IsInt({
+    message: "Los dias a vencer debe ser un número entero",
+  })
+  @Min(0, {
+    message: "Los dias a vencer no puede ser menor a 0",
+  })
+  @Max(60, {
+    message: "Los dias a vencer no puede ser mayor a 60",
+  })
+  diasavencer: number;
+
+     @IsEnum(SemaforoEnum, {
+    message: "El semaforo no es válido",
+  })
+  semaforo: SemaforoEnum;  
+
+  @IsOptional()
+  @IsString({
+    message: "La acción sugerida debe ser texto",
+  })
+  @MaxLength(2000, {
+    message: "La acción sugerida no puede superar los 2000 caracteres",
+  })
+  accionsugerida?: string;
+}
--- src/vacaciones/entities/vacacione.entity.ts
+++ src/vacaciones/entities/vacacione.entity.ts
@@ -1,5 +1,17 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+export enum TipoEmpleadoEnum{
+    QUINCENAL="QUINCENAL",
+    SEMANAL="SEMANAL"
+}
+
+export enum SemaforoEnum{
+    CONTROLADO="CONTROLADO",
+    ATENCION="ATENCION",
+    SINSALDO="SINSALDO"
+}
+
+
 @Entity()
 export class Vacacione {
     @PrimaryGeneratedColumn()
@@ -10,20 +22,96 @@ export class Vacacione {
         unique:true
     })
     idempleado:string
+
+    @Column({
+        type:"varchar",
+        length:100
+    })
     nombre:string
-    tipoempleado:string
+
+    @Column({
+        type:"enum",
+        enum:TipoEmpleadoEnum
+    })
+    tipoempleado:TipoEmpleadoEnum
+
+    @Column({
+  type: "varchar",
+  length: 100,
+})
     area:string
+
+@Column({
+  type: "varchar",
+  length: 100,
+})
     puesto:string
-    fechaingreso:string
-    antiguedad:string
+
+    @Column({
+        type:"date"
+    })
+    fechaingreso:Date
+
+    @Column({
+        type:'int'
+    })
+    antiguedad:number
+
+    @Column({
+        type:'int'
+    })
     diasderecho:string
+
+    @Column({
+        type:"date"
+    })
     iniciocicloactual:string
+
+    @Column({
+        type:"date"
+    })
     fincicloactual:string
-    proporcionaldevengado:string
+
+    @Column({
+        type:"decimal",
+        precision:5,
+        scale:2
+    })
+    proporcionaldevengado:number
+
+    @Column({
+        type:'int'
+    })
     diastomados:string
-    saldodisponible:string
+
+    @Column({
+        type:"decimal",
+        precision:6,
+        scale:2
+    })
+    saldodisponible:number
+
+    @Column({
+        type:'int'
+    })
     diasporvencer:string
+
+
+    @Column({
+        type:'int'
+    })
     diasavencer:string
-    semaforo:string
+
+    @Column({
+       type:'enum',
+       enum:SemaforoEnum 
+    })
+    semaforo:SemaforoEnum
+
+
+    @Column({
+        type:"text",
+        nullable:true
+    })
     accionsugerida:string
 }
