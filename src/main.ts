22efc3f37896f584220f74d8e951cb3f87bec835
FLEXUS <flexuselectromecanica01@gmail.com>
2026-04-29 14:57:37 -0600

    Solucion de errores

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ const port = process.env.PORT || 4008
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(
-    {
-      origin: '*', 
-    methods: '*'
-    }
-  );
+  app.enableCors({
+  origin: "https://rhquiosco.vercel.app",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+});
 
   app.setGlobalPrefix('api')
   await app.listen(port,'0.0.0.0');
