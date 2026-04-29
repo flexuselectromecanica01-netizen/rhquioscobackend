445d16d750ad182f3f9f6fa33dd85f509314f437
FLEXUS <flexuselectromecanica01@gmail.com>
2026-04-29 15:58:46 -0600

    Solicitud de vacaciones

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
   origin: "https://rhquiosco.vercel.app",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
