440d6810c0e63f6eba0eb15169c45b9c428e9a3a
FLEXUS <flexuselectromecanica01@gmail.com>
2026-04-29 16:04:06 -0600

    Solicitud de vacaciones

--- src/vacaciones/vacaciones.module.ts
+++ src/vacaciones/vacaciones.module.ts
@@ -6,10 +6,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from '../config/typeorm.config';
 import { Vacacione } from './entities/vacacione.entity';
 import { Login } from '../login/entities/login.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Vacacione,Login])
+    TypeOrmModule.forFeature([Vacacione,Login]),
+    JwtModule.register({
+          secret:"secret",
+          signOptions:{
+            expiresIn:"1d"
+          }
+        })
   ],
   controllers: [VacacionesController],
   providers: [VacacionesService],
